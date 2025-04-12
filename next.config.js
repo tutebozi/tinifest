17e57b740af7bbd5f176815a4b79b6294ca359f8
Matias Bozinovic <tu.email@ejemplo.com>
2025-04-12 18:29:15 -0300

    Add domain specific configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,25 @@ const nextConfig = {
   images: {
     domains: ['www.mercadopago.com'],
     unoptimized: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'tinifest.com',
+          },
+        ],
+        permanent: true,
+        destination: 'https://www.tinifest.com/',
+      },
+    ];
+  },
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'https://www.tinifest.com',
+    NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN || 'tinifest.com'
   }
 }
 
