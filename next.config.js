b647914ac61b3c50ba2784255b63399353918566
Matias Bozinovic <tu.email@ejemplo.com>
2025-04-12 18:59:18 -0300

    Reset configuration to basic setup

--- next.config.js
+++ next.config.js
@@ -4,25 +4,8 @@ const nextConfig = {
     domains: ['www.mercadopago.com'],
     unoptimized: true
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'tinifest.com',
-          },
-        ],
-        permanent: true,
-        destination: 'https://www.tinifest.com/',
-      },
-    ];
-  },
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'https://www.tinifest.com',
-    NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN || 'tinifest.com'
-  }
+  trailingSlash: false,
+  reactStrictMode: true
 }
 
 module.exports = nextConfig 
