e006540799712a2870dd73f9e86b6db44e50c3f1
Matias Bozinovic <tu.email@ejemplo.com>
2025-04-12 18:25:40 -0300

    Simplify next.config.js to minimum

--- next.config.js
+++ next.config.js
@@ -1,45 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*',
-      }
-    ],
     domains: ['www.mercadopago.com'],
     unoptimized: true
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          }
-        ],
-      },
-    ]
-  },
-  poweredByHeader: false,
-  reactStrictMode: true,
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'tinifest.com',
-            },
-          ],
-          destination: 'https://www.tinifest.com/:path*',
-        },
-      ],
-    }
   }
 }
 
