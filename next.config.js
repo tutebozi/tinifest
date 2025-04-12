a7d177b61d8a13437a21c21c89216973c905eab6
Matias Bozinovic <tu.email@ejemplo.com>
2025-04-12 18:00:04 -0300

    Simplify configuration and use Vercel domain temporarily

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['media.vogue.mx', 'media.glamour.mx', 'yt3.googleusercontent.com', 'www.rollingstone.com', 'www.billboard.com', 'cirquemessi.com', 'assets.goal.com', 'www.clarin.com', 'www.infobae.com', 'cloudfront-us-east-1.images.arcpublishing.com', 'www.mercadopago.com', 'www.tinifest.com'],
-    unoptimized: true
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      }
+    ],
   }
 }
 
